617123ddb120d43f86f856032dcfb57a3631e161
Jibble330 <jettb6@gmail.com>
2022-11-03 21:28:16 -0700

    finish sync and remove

--- crtsync.go
+++ crtsync.go
@@ -17,7 +17,7 @@ import (
 
 type command struct {
     Name string
-    Path string
+    Filename string
     Key string
     Loop bool
     LoopDelay int //In milliseconds
@@ -27,7 +27,32 @@ type helpcommand struct {
     Name, Help string
 }
 
-var dir string
+var (
+    dir string
+    store string
+    index []command
+
+    keys = []string{"power", "volumeup", "stop", "previous", "playpause", "next", "down", "volumedown", "up", "equal", "start", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+)
+
+func init() {
+    _, filepath, _, _ := runtime.Caller(0)
+    dir = path.Dir(filepath)
+    store = path.Join(dir, "store")
+
+    indexfile, err := os.ReadFile(path.Join(store, "index.json"))
+    if err != nil {
+        if os.IsNotExist(err) {
+            return
+        }
+        panic(err)
+    }
+
+    err = json.Unmarshal(indexfile, &index)
+    if err != nil {
+        fmt.Println("failed to unmarshal index.json. ", err)
+    }
+}
 
 func main() {
     cli.RegisterCommand("sync", sync)
@@ -41,23 +66,35 @@ func main() {
     }
 
     cli.Exec()
-
-    _, filepath, _, _ := runtime.Caller(0)
-    dir = path.Dir(filepath)
-    fmt.Println(dir)
 }
 
 func help() {
-    commands := []helpcommand{}
+    index := []helpcommand{}
     if len(cli.ARGS) > 0 {
         com := cli.ARGS[0]
-        for _, c := range commands {
+        for _, c := range index {
             if c.Name == com {
                 fmt.Println(c.Help)
                 return
             }
         }
         fmt.Println("Please ")
+    }
+}
+
+func writeindex() {
+    bytes, err := json.Marshal(index)
+    if err != nil {
+        fmt.Println("failed to marshal json. ", err)
+    }
+    
+    os.WriteFile(path.Join(store, "index.json"), bytes, 0644)
+    if err != nil {
+        if os.IsNotExist(err) {
+            fmt.Println("index file does not exist. please run init")
+        } else {
+            fmt.Println("failed to write to index file. ", err)
+        }
     }
 }
 
@@ -73,12 +110,12 @@ func storeinit(args, flags []string) {
         fmt.Println("key file does not exist")
     }
 
-    err = os.Mkdir(path.Join(dir, "store"), os.ModeDir)
+    err = os.Mkdir(store, os.ModeDir)
     if err != nil && !os.IsExist(err) {
         panic(err)
     }
 
-    file, err := os.Create(path.Join(dir, "store", "index.json"))
+    file, err := os.Create(path.Join(store, "index.json"))
     if err != nil && !os.IsExist(err) {
         panic(err)
     }
@@ -89,7 +126,7 @@ func storeinit(args, flags []string) {
         panic(err)
     }
 
-    keyfile, err := os.Create(path.Join(dir, "store", "id_rsa"))
+    keyfile, err := os.Create(path.Join(store, "id_rsa"))
     if err != nil {
         panic(err)
     }
@@ -106,30 +143,13 @@ func storeinit(args, flags []string) {
 }
 
 func list(args, flags []string) {
-    filestr, err := os.ReadFile(path.Join(dir, "store/index.json"))
-    if err != nil {
-        if os.IsNotExist(err) {
-            fmt.Println("index file does not exist. please run the init command to initialize the directory.")
-            os.Exit(1)
-        }
-        panic(err)
-    }
-
-    bytes := []byte(filestr)
-
-    var commands []command
-    err = json.Unmarshal(bytes, &commands)
-    if err != nil {
-        panic(err)
-    }
-
-    maxlen := [4]int{len("Name"), len("Key"), len("Path"), len("Loop")}
-    for _, c := range commands {
+    maxlen := [4]int{len("Name"), len("File"), len("Key"), len("Loop")}
+    for _, c := range index {
         if len(c.Name) > maxlen[0] {
             maxlen[0] = len(c.Name)
         }
-        if len(c.Path) > maxlen[1] {
-            maxlen[1] = len(c.Path)
+        if len(c.Filename) > maxlen[1] {
+            maxlen[1] = len(c.Filename)
         }
         if len(c.Key) > maxlen[2] {
             maxlen[2] = len(c.Key)
@@ -139,74 +159,87 @@ func list(args, flags []string) {
         }
     }
 
-    fmt.Printf(" %v | %v | %v | %v \n", padding.Pad("Name", maxlen[0], padding.EDGES), padding.Pad("Path", maxlen[1], padding.EDGES), padding.Pad("Key", maxlen[2], padding.EDGES), padding.Pad("Loop", maxlen[3], padding.EDGES))
+    fmt.Printf(" %v | %v | %v | %v \n", padding.Pad("Name", maxlen[0], padding.EDGES), padding.Pad("File", maxlen[1], padding.EDGES), padding.Pad("Key", maxlen[2], padding.EDGES), padding.Pad("Loop", maxlen[3], padding.EDGES))
     fmt.Printf("-%v-|-%v-|-%v-|-%v-\n", padding.Fill("", maxlen[0], '-', padding.RIGHT), padding.Fill("", maxlen[1], '-', padding.RIGHT), padding.Fill("", maxlen[2], '-', padding.RIGHT), padding.Fill("", maxlen[3], '-', padding.RIGHT))
-    for _, c := range commands {
-        fmt.Printf(" %v | %v | %v | %v \n", padding.Pad(c.Name, maxlen[0], padding.RIGHT), padding.Pad(c.Path, maxlen[1], padding.RIGHT), padding.Pad(c.Key, maxlen[2], padding.RIGHT), padding.Pad(c.Loop, maxlen[3], padding.RIGHT))
+    for _, c := range index {
+        fmt.Printf(" %v | %v | %v | %v \n", padding.Pad(c.Name, maxlen[0], padding.RIGHT), padding.Pad(c.Filename, maxlen[1], padding.RIGHT), padding.Pad(c.Key, maxlen[2], padding.RIGHT), padding.Pad(c.Loop, maxlen[3], padding.RIGHT))
     }
 }
 
 func add(args, flags []string) {
     if len(args) < 3 {
         fmt.Println("incorrect usage. requires at least the name, key, and path of the command")
+        os.Exit(1)
     }
 
-    name, animpath, key := args[0], args[1], args[2]
+    name, filename, key := args[0], args[1], args[2]
     loop := cli.Bool("loop", false)
     loopdelay := cli.Int("delay", 0)
 
-    cmd := command{name, animpath, key, loop, loopdelay}
-
-    filestr, err := os.ReadFile(path.Join(dir, "store/index.json"))
-    if err != nil {
-        if os.IsNotExist(err) {
-            fmt.Println("index file does not exist. please run the init command to initialize the directory.")
-            os.Exit(1)
+    var valid bool
+    for _, k := range keys {
+        if k == key {
+            valid = true
+            break
         }
-        panic(err)
+    }
+    if !valid {
+        fmt.Println("invalid key. valid keys are ", keys)
+        os.Exit(1)
     }
 
-    bytes := []byte(filestr)
+    cmd := command{name, filename, key, loop, loopdelay}
 
-    var commands []command
-    err = json.Unmarshal(bytes, &commands)
-    if err != nil {
-        panic(err)
-    }
-    
-    for _, c := range commands {
+    for _, c := range index {
         if cmd.Name == c.Name || cmd.Key == c.Key {
             fmt.Println("key and name must be unique")
             os.Exit(1)
         }
     }
+    index = append(index, cmd)
 
-    commands = append(commands, cmd)
-    fmt.Println(cmd, commands)
-
-    
-
-    marshaled, err := json.Marshal(commands)
-    if err != nil {
-        panic(err)
-    }
-    
-    err = os.WriteFile(path.Join(dir, "store/index.json"), marshaled, 0644)
-    if err != nil {
-        if os.IsNotExist(err) {
-            fmt.Println("index file does not exist. please run the init command to initialize the directory.")
-            os.Exit(1)
-        }
-        panic(err)
-    }
+    writeindex()
 }
 
 func remove(args, flags []string) {
+    if len(args) < 1 {
+        fmt.Println("name is a required argument.")
+        os.Exit(1)
+    }
 
+    name := args[0]
+
+    i := -1
+    for pos, c := range index {
+        if c.Name == name {
+            i = pos
+            break
+        }
+    }
+    if i == -1 {
+        fmt.Println("no command with the given name exists. use crtsync list to see registered index.")
+        os.Exit(1)
+    }
+
+    index = append(index[:i], index[i+1:]...)
+
+    writeindex()
+}
+
+func exists(path string) bool {
+    _, err := os.Stat(path)
+    return !os.IsNotExist(err)
 }
 
 func sync(args, flags []string) {
-    clientConfig, _ := auth.PrivateKey("pi", path.Join(dir, "store", "id_rsa"), ssh.InsecureIgnoreHostKey())
+    for _, c := range index {
+        if !exists(path.Join(store, c.Filename)) {
+            fmt.Printf("missing file %v for %v.\n", c.Filename, c.Name)
+        }
+        os.Exit(1)
+    }
+    
+    clientConfig, _ := auth.PrivateKey("pi", path.Join(store, "id_rsa"), ssh.InsecureIgnoreHostKey())
 
     client := scp.NewClient("raspberrypi:22", &clientConfig)
 
@@ -217,10 +250,19 @@ func sync(args, flags []string) {
 	}
     defer client.Close()
     
-    f, _ := os.Open(path.Join(dir, "store", "index.json"))
+    f, _ := os.Open(path.Join(store, "index.json"))
     defer f.Close()
     err = client.CopyFromFile(context.Background(), *f, "/home/pi/Documents/Scripts/CRT/CRT/store/index.json", "0655")
     if err != nil {
         panic(err)
+    }
+
+    for _, c := range index {
+        f, _ = os.Open(path.Join(store, c.Filename))
+        defer f.Close()
+        err = client.CopyFromFile(context.Background(), *f, "/home/pi/Documents/Scripts/CRT/CRT/store/" + c.Filename, "0655")
+        if err != nil {
+            panic(err)
+        }
     }
 }
